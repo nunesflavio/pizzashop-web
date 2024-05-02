6c1021baf8086bace710d2f7cf943adc555295a4
nunesflavio <pnunesflavio@gmail.com>
2024-05-02 17:42:44 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './global.css'
+
+import { Button } from '@/components/ui/button.tsx'
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center ">
+      <Button> Envia </Button>
+    </div>
+  )
+}
