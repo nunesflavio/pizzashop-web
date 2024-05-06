64b19b4840c7b4e84ac52d35ee28e976b46332f8
nunesflavio <pnunesflavio@gmail.com>
2024-05-06 06:44:45 -0300

    theme and head

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from '@/routes'
+import { ThemeProvider } from '@/components/theme/theme-provider.tsx'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
