2297eb60a5554aafcc1d2c748debd5bbc6cfc1d5
nunesflavio <pnunesflavio@gmail.com>
2024-05-07 15:21:04 -0300

    dashboard

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,31 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrdersAmountCard } from '@/pages/app/dashboard/day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from '@/pages/app/dashboard/month-canceled-orders-amount-card.tsx'
+import { MonthOrdersAmountCard } from '@/pages/app/dashboard/month-orders-amount-card'
+import { MonthRevenueCard } from '@/pages/app/dashboard/month-revenue-card'
+import { PopularProductsChart } from '@/pages/app/dashboard/popular-products-chart.tsx'
+import { RevenueChart } from '@/pages/app/dashboard/revenue-chart.tsx'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashborad</h1>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4">
+        <MonthRevenueCard />
+        <MonthOrdersAmountCard />
+        <DayOrdersAmountCard />
+        <MonthCanceledOrdersAmountCard />
+      </div>
+
+      <div className="grid grid-cols-9 gap-4">
+        <RevenueChart />
+        <PopularProductsChart />
+      </div>
+    </>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,15 +2,21 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from '@/pages/_layouts/app.tsx'
 import { AuthLayout } from '@/pages/_layouts/auth.tsx'
-import { Dashboard } from '@/pages/app/dashboard.tsx'
+import { NotFound } from '@/pages/404.tsx'
+import { Dashboard } from '@/pages/app/dashboard/dashboard.tsx'
 import { SignIn } from '@/pages/auth/sign-in.tsx'
-import {SignUp} from "@/pages/auth/sign-up.tsx";
+import { SignUp } from '@/pages/auth/sign-up.tsx'
+import { Orders } from '@/pages/orders/orders.tsx'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    errorElement: <NotFound />,
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
+    ],
   },
 
   {
